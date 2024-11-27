2a9a7da104543ac9e2eab030e95878a482e61104
Chau Nguyen <chaunguyenm@doofenshmirtz.local>
2024-11-27 02:31:24 -0500

    added partial ffstft

--- src/cpp/fft.cc
+++ src/cpp/fft.cc
@@ -4,48 +4,34 @@
 
 #define PI 3.14159265
 
-std::vector<std::vector<std::complex<double>>> fft::stft_dft(
-    std::vector<std::complex<double>> &vec, size_t window_size, size_t window_step)
+rarray<std::complex<double>, 2> fft::stft_dft(
+    rarray<std::complex<double>, 1> &vec, size_t window_size, size_t window_step)
 {
-  std::vector<std::vector<std::complex<double>>> spectrogram = std::vector<std::vector<std::complex<double>>>();
-  for (size_t begin = 0; begin < vec.size() - window_size; begin += window_step)
+  size_t spectrogram_size = ceil((vec.size() - window_size) / float(window_step));
+  rarray<std::complex<double>, 2> spectrogram(spectrogram_size, window_size);
+
+#pragma omp parallel for default(none) shared(vec, window_size, window_step, spectrogram, spectrogram_size)
+  for (size_t i = 0; i < spectrogram_size; i++)
   {
-    spectrogram.push_back(fft::dft(vec, begin, window_size));
+    rarray<std::complex<double>, 1> dft = fft::dft(vec, i * window_step, window_size);
+
+#pragma omp parallel for default(none) shared(window_size, dft, i, spectrogram)
+    for (size_t j = 0; j < window_size; j++)
+      spectrogram[i][j] = dft[j];
   }
   return spectrogram;
 }
 
-std::vector<std::vector<std::complex<double>>> fft::stft_fft(
-    std::vector<std::complex<double>> &vec, size_t window_size, size_t window_step)
-{
-  std::vector<std::vector<std::complex<double>>> spectrogram = std::vector<std::vector<std::complex<double>>>();
-  for (size_t begin = 0; begin < vec.size() - window_size; begin += window_step)
-  {
-    spectrogram.push_back(fft::fft(vec, begin, window_size));
-  }
-  return spectrogram;
-}
-
-std::vector<std::vector<std::complex<double>>> fft::stft2_fft(
-    std::vector<std::complex<double>> &vec, size_t window_size, size_t window_step)
-{
-  size_t num_stages = log2(window_size) + 1;
-  rarray<std::complex<double>, 2> M(window_size / 2, window_size / 2 - 1),
-      M_prime(window_size / 2, window_size / 2 - 1), fft0(window_size, num_stages);
-  
-  // Initialize M
-  transformRadix2(vec, fft0);
-  for (size_t s = 0; s < num_stages; s++)
-    for (size_t k = 0; k < window_size / pow(2, s + 1); k++)
-      M[s][k] = fft0[s][2 * k + 1];
-}
-
-std::vector<std::complex<double>> fft::dft(
-    const std::vector<std::complex<double>> &signal,
+rarray<std::complex<double>, 1> fft::dft(
+    const rarray<std::complex<double>, 1> &signal,
     size_t vec_begin, size_t vec_size)
 {
-  std::vector<std::complex<double>> output;
-  for (size_t k = 0; k < vec_size / 2.; k++)
+  if (vec_size == 0)
+    vec_size = signal.size();
+  size_t vec_end = vec_begin + vec_size;
+
+  rarray<std::complex<double>, 1> output(vec_size);
+  for (size_t k = 0; k < vec_size; k++)
   {
     std::complex<double> sum = 0.;
     for (size_t t = 0; t < vec_size; t++)
@@ -53,24 +39,131 @@ std::vector<std::complex<double>> fft::dft(
       float angle = 2 * PI * t * k / vec_size;
       sum += signal[t + vec_begin] * std::exp(std::complex<double>(0, -angle));
     }
-    output.push_back(sum);
+    output[k] = sum;
   }
   return output;
 }
 
-std::vector<std::complex<double>> fft::fft(const std::vector<std::complex<double>> &signal, size_t vec_begin, size_t vec_size)
+rarray<std::complex<double>, 2> fft::stft_fft(
+    rarray<std::complex<double>, 1> &vec, size_t window_size, size_t window_step)
 {
-  std::vector<std::complex<double>>::const_iterator first = signal.begin() + vec_begin;
-  std::vector<std::complex<double>>::const_iterator last = signal.begin() + vec_begin + vec_size;
-  std::vector<std::complex<double>> subsignal(first, last);
+  size_t spectrogram_size = ceil((vec.size() - window_size) / float(window_step));
+  rarray<std::complex<double>, 2> spectrogram(spectrogram_size, window_size);
 
-  rarray<std::complex<double>, 2> fft(8, 4);
-fft:
-  transformRadix2(subsignal, fft);
+#pragma omp parallel for default(none) shared(vec, window_size, window_step, spectrogram, spectrogram_size)
+  for (size_t i = 0; i < spectrogram_size; i++)
+  {
+    rarray<std::complex<double>, 1> fft = fft::fft(vec, i * window_step, window_size);
+
+#pragma omp parallel for default(none) shared(window_size, fft, i, spectrogram)
+    for (size_t j = 0; j < window_size; j++)
+      spectrogram[i][j] = fft[j];
+  }
+  return spectrogram;
+}
+
+rarray<std::complex<double>, 2> fft::stft(
+    rarray<std::complex<double>, 1> &vec, size_t window_size, size_t window_step)
+{
+  /*
+   * STEP 1: Create buffers M and M'
+   */
+  size_t num_stages = log2(window_size) + 1;
+  size_t spectrogram_size = ceil((vec.size() - window_size) / float(window_step));
+  size_t buffer_rows = window_size / 2;
+  size_t buffer_cols = window_size / 2 - 1;
+
+  rarray<std::vector<std::complex<double>>, 2> M(buffer_rows, buffer_cols),
+      M_prime(buffer_rows, buffer_cols);
+  rarray<std::complex<double>, 2> fft0(window_size, num_stages),
+      spectrogram(spectrogram_size, window_size);
+  rarray<std::complex<double>, 1> exptable(window_size / 2);
+
+  /*
+   * STEP 2: Initialize M
+   */
+  // Perform FFT on 1st N signals
+  rarray<std::complex<double>, 1> subvec(window_size);
+  for (size_t i = 0; i < window_size; i++)
+    subvec[i] = vec[i];
+  transform(subvec, fft0, exptable);
+  std::cout << fft0 << "\n";
+  // Set M to initial FFT
+  for (size_t i = 0; i < window_size / 2; i++)
+    M[i][0].push_back(vec[i + window_size / 2]);
+  for (size_t s = 1; s < num_stages - 1; s++)
+    for (size_t k = 0; k < window_size / pow(2, s + 1); k++)
+      for (size_t m = 0; m < pow(2, s); m++)
+        M[k][s].push_back(fft0[(2 * k + 1) * pow(2, s) + m][s]);
+
+  for (size_t r = 0; r < buffer_rows; r++)
+  {
+    for (size_t c = 0; c < buffer_cols; c++)
+    {
+      std::cout << r << "," << c << "=[";
+      for (const auto &elem : M[r][c])
+        std::cout << elem << ",";
+      std::cout << "] ";
+    }
+    std::cout << "\n";
+  }
+
+  /*
+   * STEP 3: Compute batches of N/2 FFTs
+   */
+  for (size_t n = 1; n < vec.size() - window_size + 1; n++)
+    for (size_t s = 1; s < num_stages; s++)
+    {
+      // Initialize temporary input buffer
+      rarray<std::vector<std::complex<double>>, 1> B0(window_size / 2);
+      rarray<std::vector<std::complex<double>>, 1> B1(window_size / 2);
+      rarray<size_t, 1> B2(window_size / 2);
+
+      for (size_t i = 0; i < window_size / 2; i++)
+      {
+        B0[i].push_back(vec[i + n + window_size - 1]);
+        B1[i] = M[i][s - 1];
+        B2[i] = s;
+
+        // std::cout << "B0[" << i << "]=" << vec[i + n + window_size - 1] << "\n";
+        // std::cout << "B1[" << i << "]=";
+        // for (const auto &element : M[i][s - 1])
+        //   std::cout << element << ", ";
+        // std::cout << "\n";
+        // std::cout << "B2[" << i << "]=" << s << "\n";
+      }
+
+      // std::cout << B0 << "\n";
+      // std::cout << B1 << "\n";
+      // std::cout << B2 << "\n";
+
+      // Parallel
+      for (size_t k = 0; k < pow(2, s) - 2; k++)
+      {
+        std::complex<double> xr = B0[0][k] * std::polar(1.0, -2. * PI * 0 / n);
+        std::complex<double> x0 = B1[0][k] + xr;
+        std::complex<double> x1 = B1[0][k] - xr;
+
+        // std::cout << x0 << " " << x1 << "\n";
+      }
+    }
+
+  return spectrogram;
+}
+
+rarray<std::complex<double>, 1> fft::fft(
+    const rarray<std::complex<double>, 1> &signal,
+    size_t vec_begin, size_t vec_size)
+{
+  rarray<std::complex<double>, 1> subsignal(vec_size);
+  for (size_t i = 0; i < vec_size; i++)
+    subsignal[i] = signal[vec_begin + i];
+
+  fft::transform(subsignal);
   return subsignal;
 }
 
-bool fft::transformRadix2(std::vector<std::complex<double>> &vec)
+bool fft::transform(rarray<std::complex<double>, 1> &vec)
 {
   // Length variables
   size_t n = vec.size();
@@ -81,7 +174,7 @@ bool fft::transformRadix2(std::vector<std::complex<double>> &vec)
     return false; // n is not a power of 2
 
   // Trigonometric tables
-  std::vector<std::complex<double>> exptable(n / 2);
+  rarray<std::complex<double>, 1> exptable(n / 2);
   for (size_t i = 0; i < n / 2; i++)
     exptable[i] = std::polar(1.0, -2. * PI * i / n);
 
@@ -119,8 +212,9 @@ bool fft::transformRadix2(std::vector<std::complex<double>> &vec)
   return true;
 }
 
-bool fft::transformRadix2(std::vector<std::complex<double>> &vec,
-                          rarray<std::complex<double>, 2> &fft)
+bool fft::transform(rarray<std::complex<double>, 1> &vec,
+                    rarray<std::complex<double>, 2> &fft,
+                    rarray<std::complex<double>, 1> &exptable)
 {
   // Length variables
   size_t n = vec.size();
@@ -131,7 +225,7 @@ bool fft::transformRadix2(std::vector<std::complex<double>> &vec,
     return false; // n is not a power of 2
 
   // Trigonometric tables
-  std::vector<std::complex<double>> exptable(n / 2);
+  // rarray<std::complex<double>, 1> exptable(n / 2);
   for (size_t i = 0; i < n / 2; i++)
     exptable[i] = std::polar(1.0, -2. * PI * i / n);
 
@@ -164,7 +258,10 @@ bool fft::transformRadix2(std::vector<std::complex<double>> &vec,
         vec[j] += temp;
         fft[j][log2(size)] = vec[j];
         fft[l][log2(size)] = vec[l];
+        // std::cout << j << "," << log2(size) << "=" << exptable[k] << " ";
+        // std::cout << l << "," << log2(size) << "=" << exptable[k] << " ";
       }
+      // std::cout << "\n";
     }
     if (size == n) // Prevent overflow in 'size *= 2'
       break;
--- src/cpp/main.cc
+++ src/cpp/main.cc
@@ -4,40 +4,54 @@
 
 int main()
 {
-  std::vector<std::complex<double>> signal = {
-    std::complex<double>(0.0, 0.0),
-    std::complex<double>(1.0, 0.0),
-    std::complex<double>(2.0, 0.0),
-    std::complex<double>(3.0, 0.0),
-    std::complex<double>(4.0, 0.0),
-    std::complex<double>(5.0, 0.0),
-    std::complex<double>(6.0, 0.0),
-    std::complex<double>(7.0, 0.0),
-    std::complex<double>(8.0, 0.0),
-    std::complex<double>(9.0, 0.0),
-    std::complex<double>(10.0, 0.0),
-    std::complex<double>(11.0, 0.0),
-    std::complex<double>(12.0, 0.0),
-    std::complex<double>(13.0, 0.0),
-    std::complex<double>(14.0, 0.0),
-    std::complex<double>(15.0, 0.0)
-  };
-  std::vector<std::vector<std::complex<double>>> spectrogram1 = fft::stft_dft(signal, 8, 2);
-  std::cout << "Spectrogram matrix:" << std::endl;
-  for (const auto& row : spectrogram1) {
-    for (const auto& element : row) {
-      std::cout << "(" << element.real() << ", " << element.imag() << ") ";
-    }
-    std::cout << std::endl;
-  }
+  size_t size = 16;
+  rarray<std::complex<double>, 1> signal(size);
+  for (size_t i = 0; i < size; i++)
+    signal[i] = std::complex<double>(double(i), 0.0);
+  // std::cout << signal << "\n";
 
-  std::vector<std::vector<std::complex<double>>> spectrogram2 = fft::stft_fft(signal, 8, 2);
-  std::cout << "Spectrogram matrix:" << std::endl;
-  for (const auto& row : spectrogram2) {
-    for (const auto& element : row) {
-      std::cout << "(" << element.real() << ", " << element.imag() << ") ";
-    }
-    std::cout << std::endl;
-  }
-  return 0; 
+  // rarray<std::complex<double>, 2> stft_dft = fft::stft_dft(signal, 8, 2);
+  // std::cout << stft_dft << "\n";
+
+  // rarray<std::complex<double>, 2> stft_fft = fft::stft_fft(signal, 8, 2);
+  // std::cout << stft_fft << "\n";
+
+  fft::stft(signal, 8, 1);
+  // std::cout << stft_fft << "\n";
+  // std::vector<std::complex<double>> signal = {
+  //   std::complex<double>(0.0, 0.0),
+  //   std::complex<double>(1.0, 0.0),
+  //   std::complex<double>(2.0, 0.0),
+  //   std::complex<double>(3.0, 0.0),
+  //   std::complex<double>(4.0, 0.0),
+  //   std::complex<double>(5.0, 0.0),
+  //   std::complex<double>(6.0, 0.0),
+  //   std::complex<double>(7.0, 0.0),
+  //   std::complex<double>(8.0, 0.0),
+  //   std::complex<double>(9.0, 0.0),
+  //   std::complex<double>(10.0, 0.0),
+  //   std::complex<double>(11.0, 0.0),
+  //   std::complex<double>(12.0, 0.0),
+  //   std::complex<double>(13.0, 0.0),
+  //   std::complex<double>(14.0, 0.0),
+  //   std::complex<double>(15.0, 0.0)
+  // };
+  // std::vector<std::vector<std::complex<double>>> spectrogram1 = fft::stft_dft(signal, 8, 2);
+  // std::cout << "Spectrogram matrix:" << std::endl;
+  // for (const auto& row : spectrogram1) {
+  //   for (const auto& element : row) {
+  //     std::cout << "(" << element.real() << ", " << element.imag() << ") ";
+  //   }
+  //   std::cout << std::endl;
+  // }
+
+  // std::vector<std::vector<std::complex<double>>> spectrogram2 = fft::stft_fft(signal, 8, 2);
+  // std::cout << "Spectrogram matrix:" << std::endl;
+  // for (const auto& row : spectrogram2) {
+  //   for (const auto& element : row) {
+  //     std::cout << "(" << element.real() << ", " << element.imag() << ") ";
+  //   }
+  //   std::cout << std::endl;
+  // }
+  // return 0; 
 }
